c99b128be260737bc8860caa2c8ad1c43575b6ad
cloudwolf <206946888+pocketradio@users.noreply.github.com>
2026-07-22 08:04:57 +0530

    add tcp port flag validation

--- cmd/ridgedb/main.go
+++ cmd/ridgedb/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net"
 	"ridgeDB/internal/server"
 	"ridgeDB/internal/storage"
+	"strconv"
 )
 
 func main() {
+
+	portPtr := flag.String("port", ":8000", "TCP port")
+
+	flag.Parse()
+
+	_, port, err := net.SplitHostPort(*portPtr)
+	if err != nil {
+		log.Fatalf("invalid port %q: expected format like :8000", *portPtr)
+	}
+
+	if _, err := strconv.Atoi(port); err != nil {
+		log.Fatalf("invalid port %q: port must be a number", *portPtr)
+	}
+
+	fmt.Println("Port: ", *portPtr)
+
 	db := storage.NewStore()
 	go db.StartCleanup()
-	fmt.Println("Starting server on port 8000...")
-	listener := server.Start()
+
+	listener := server.Start(portPtr)
 	server.Accept(db, listener)
 }

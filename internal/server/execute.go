b28b79581f2193da48e9c2d1372094fabeb642f6
cloudwolf <206946888+pocketradio@users.noreply.github.com>
2026-07-22 23:51:13 +0530

    persist SET to AOF and return error on write

--- internal/server/execute.go
+++ internal/server/execute.go
@@ -1,13 +1,19 @@
 package server
 
 import (
+	"ridgeDB/internal/persistence"
 	"ridgeDB/internal/storage"
 	"time"
 )
 
-func ExecuteCommand(db *storage.Store, cmd Command) CommandResult {
+func ExecuteCommand(db *storage.Store, cmd Command, aof *persistence.AOF) (CommandResult, error) {
 	switch cmd.Method {
 	case "SET":
+
+		err := aof.AppendSet(cmd.Key, cmd.Data)
+		if err != nil {
+			return CommandResult{}, err
+		}
 		if cmd.Expiry {
 			_ = db.Set(cmd.Key, storage.Value{
 				Data:      cmd.Data,
@@ -15,7 +21,7 @@ func ExecuteCommand(db *storage.Store, cmd Command) CommandResult {
 				ExpiresAt: time.Now().Add(240 * time.Hour),
 			})
 
-			return CommandResult{Status: "OK"}
+			return CommandResult{Status: "OK"}, nil
 		}
 
 		_ = db.Set(cmd.Key, storage.Value{
@@ -23,29 +29,30 @@ func ExecuteCommand(db *storage.Store, cmd Command) CommandResult {
 			HasExpiry: cmd.Expiry,
 			ExpiresAt: time.Time{},
 		})
-		return CommandResult{Status: "OK"}
+
+		return CommandResult{Status: "OK"}, nil
 
 	case "GET":
 		val, ok := db.Get(cmd.Key)
 		if ok != true {
-			return CommandResult{HasKey: false, Status: "Key not present."}
+			return CommandResult{HasKey: false, Status: "Key not present."}, nil
 		}
 
 		return CommandResult{
 			HasKey: true,
 			Status: "Key present.",
 			Value:  val,
-		}
+		}, nil
 
 	case "DEL":
 		_ = db.Delete(cmd.Key)
 
 		return CommandResult{
 			Status: "Key deleted.",
-		}
+		}, nil
 	}
 
 	return CommandResult{
 		Status: "Please enter a valid method.",
-	}
+	}, nil
 }
